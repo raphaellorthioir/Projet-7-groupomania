99bc2db2b2634d59b3380c63dd4c2e14333e3eef
raphael.lorthioir <raphael.lorthioir@gmail.com>
2022-08-26 16:24:07 +0200

    mongo db connect

--- backend/app.js
+++ backend/app.js
@@ -5,19 +5,18 @@ const path = require('path'); // donne accés aux chemins du système de fichier
 const app = express(); // création de l'application express qui sera lu par le serveur node
 module.exports = app; // export de la constante pour y accéder depuis les autres fichiers du serveur node
 const helmet = require('helmet');
-const userRoutes = require('./routes/user');
-const saucesRoutes = require('./routes/sauces');
+/*const userRoutes = require('./routes/user');
+const saucesRoutes = require('./routes/sauces');*/
 require('dotenv').config(); // permet de cacher des variables
 
-mongoose
-  .connect(process.env.MY_MONGO_DB_LINK, {
+mongoose.connect('mongodb+srv://LaStrad:comedia1991@groupomaniadb.ksguq0n.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-//app.use(express.json());//intercèpte les requêtes qui ont un content type json  et nous met à disposition le corps de la requête dans req.body
+app.use(express.json());//intercèpte les requêtes qui ont un content type json  et nous met à disposition le corps de la requête dans req.body
 
 // obligatoire pour éviter les problèmes de CORS et permettre à l'application d'accéder au serveur , il faut préciser les autorisations
 app.use((req, res, next) => {
@@ -36,7 +35,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-//app.use(helmet()) // action par défaut pour la sécurité blbla
+//app.use(helmet()) // action par défaut pour la sécurité 
 
 /* app.use('/images',express.static(path.join(__dirname, 'images')))//  requêtes vers le dossier local  '/images' , on utilise static pour servir le dossier image, on définit la route avec path.join en indiquant le nom du dossier
   app.use('/api/auth',userRoutes);
