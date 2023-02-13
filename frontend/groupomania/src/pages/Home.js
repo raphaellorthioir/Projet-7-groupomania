ee52ef161b161b5f6236b332bb200791ed96a1b3
raphael.lorthioir <raphael.lorthioir@gmail.com>
2023-02-13 14:33:03 +0100

    start: infinite scroll

--- frontend/groupomania/src/pages/Home.js
+++ frontend/groupomania/src/pages/Home.js
@@ -1,29 +1,31 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
 import { UserContext } from '../components/AppContext';
-import Navbar from '../components/Navbar';
 import Post from '../components/Posts/Post';
 import axios from 'axios';
 import CreatePost from '../components/Posts/CreatePost';
+import { Navigate } from 'react-router-dom';
 
 const Home = () => {
   const user = useContext(UserContext);
   const [posts, setPosts] = useState();
   const [wantCreatePost, setWantCreatePost] = useState(false);
+  const [numberOfPosts, setNumberOfPosts] = useState(0);
   const fetchPosts = async () => {
     await axios
       .get(`${process.env.REACT_APP_API_URL}api/post`, {
         withCredentials: true,
       })
       .then((res) => {
-        setPosts(res.data);
+       const data= res.data.slice(numberOfPosts,numberOfPosts + 5)
+       console.log(data)
+        setPosts(data);
       })
       .catch((err) => {
         console.log(err);
       });
   };
   const updatePosts = () => {
-    fetchPosts();
+    fetchPosts(numberOfPosts);
   };
 
   useEffect(() => {
@@ -33,38 +35,45 @@ const Home = () => {
   const switchCreatePost = () => {
     setWantCreatePost(true);
   };
-const unSwitchCreatePost =()=>{
-  setWantCreatePost(false)
-}
+  const unSwitchCreatePost = () => {
+    setWantCreatePost(false);
+  };
   return (
     <>
-      <main>
-        {wantCreatePost ? (
-          <CreatePost unSwitchCreatePost={unSwitchCreatePost} updatePosts={updatePosts} />
-        ) : (
-          <section className="flex ac-center newPostContainer">
-            <div className="create-container">
-              <div className="flex row ai-center ac-center space-around create-box">
-                <img
-                  className="profilPicture"
-                  src={user?.profilPicture}
-                  alt=""
-                />
-                <div className="createSwitch" onClick={switchCreatePost}>
-                  Quoi de neuf, {user?.pseudo} ?
+      {user ? (
+        <main>
+          {wantCreatePost ? (
+            <CreatePost
+              unSwitchCreatePost={unSwitchCreatePost}
+              updatePosts={updatePosts}
+            />
+          ) : (
+            <section className="flex ac-center newPostContainer">
+              <div className="create-container">
+                <div className="flex row ai-center ac-center space-around create-box">
+                  <img
+                    className="profilPicture"
+                    src={user?.profilPicture}
+                    alt=""
+                  />
+                  <div className="createSwitch" onClick={switchCreatePost}>
+                    Quoi de neuf, {user?.pseudo} ?
+                  </div>
                 </div>
               </div>
-            </div>
-          </section>
-        )}
+            </section>
+          )}
 
-        <div className="post-container  ">
-          {posts &&
-            posts.map((item) => (
-              <Post post={item} updatePosts={updatePosts} key={item._id} />
-            ))}
-        </div>
-      </main>
+          <div className="post-container  ">
+            {posts &&
+              posts.map((item) => (
+                <Post post={item} updatePosts={updatePosts} key={item._id} />
+              ))}
+          </div>
+        </main>
+      ) : (
+        <Navigate to="/signing" />
+      )}
     </>
   );
 };
